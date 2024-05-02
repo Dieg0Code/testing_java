c48f5502938d0e72e2f7f9bbeb01b70eb7feb201
Dieg0Code <diegoobando20@gmail.com>
2024-05-01 23:01:26 -0400

    spring boot test

---
+++ sprinboot_test/src/main/java/com/dieg0code/sprinboot_test/SprinbootTestApplication.java
@@ -0,0 +1,13 @@
+package com.dieg0code.sprinboot_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootTestApplication.class, args);
+	}
+
+}
